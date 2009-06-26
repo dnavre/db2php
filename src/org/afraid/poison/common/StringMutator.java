9ad1c079579dc125f004e9d1d346538f80421b63
poison <rc.poison@gmail.com>
2009-06-26 20:18:11 +0200

    ADD: PHP code for modification tracking

---
+++ src/org/afraid/poison/common/StringMutator.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.afraid.poison.common;
+
+/**
+ *
+ * @author andreas.schnaiter
+ */
+public interface StringMutator {
+	public String transform(Object s);
+}
---
+++ src/org/afraid/poison/db2php/generator/CODE_MODIFICATION_TRACKING.php
@@ -0,0 +1,31 @@
+	private $oldInstance=null;
+
+	public function getOldInstance() {
+		return $this->oldInstance;
+	}
+	/**
+	 * called when the field with the passed id has changed
+	 *
+	 * @param int $fieldId
+	 */
+	protected function notifyChanged($fieldId) {
+		if (is_null($this->getOldInstance())) {
+			$this->oldInstance=$this;
+		}
+	}
+
+	/**
+	 * return true if this instance has been modified since the last setPristine() call
+	 *
+	 * @return bool
+	 */
+	public function isChanged() {
+		return !is_null($this->getOldInstance());
+	}
+
+	/**
+	 * set this instance into pristine state
+	 */
+	public function setPristine() {
+		$this->oldInstance=null;
+	}
