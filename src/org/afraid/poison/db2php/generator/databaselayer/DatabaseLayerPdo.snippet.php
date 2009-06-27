f2af309119ea8ecccfbfc00e43007cbc27c50a47
poison <rc.poison@gmail.com>
2009-06-27 03:13:29 +0200

    ADD: db snippet for PDO

---
+++ src/org/afraid/poison/db2php/generator/databaselayer/DatabaseLayerPdo.snippet.php
@@ -0,0 +1,60 @@
+	/**
+	 * cached insert statement
+	 *
+	 * @var PDOStatement
+	 */
+	private static $stmtInsert=null;
+	/**
+	 * cached update statement
+	 *
+	 * @var PDOStatement
+	 */
+	private static $stmtUpdate=null;
+	/**
+	 * cached delete statement
+	 *
+	 * @var PDOStatement
+	 */
+	private static $stmtDelete=null;
+	/**
+	 * cached select statement
+	 *
+	 * @var PDOStatement
+	 */
+	private static $stmtSelect=null;
+	private static $cacheStatements=true;
+
+	protected static function prepareStatement(PDO $db, $statement) {
+		if(self::isCacheStatements()) {
+			if ($statement==self::SQL_INSERT) {
+				if (null==self::$stmtInsert) {
+					self::$stmtInsert=$db->prepare($statement);
+				}
+				return self::$stmtInsert;
+			} else if($statement==self::SQL_UPDATE) {
+				if (null==self::$stmtInsert) {
+					self::$stmtInsert=$db->prepare($statement);
+				}
+				return self::$stmtInsert;
+			} else if($statement==self::SQL_SELECT_PK) {
+				if (null==self::$stmtInsert) {
+					self::$stmtInsert=$db->prepare($statement);
+				}
+				return self::$stmtInsert;
+			} else if($statement==self::SQL_DELETE_PK) {
+				if (null==self::$stmtInsert) {
+					self::$stmtInsert=$db->prepare($statement);
+				}
+				return self::$stmtInsert;
+			}
+		}
+		return $db->prepare($statement);
+	}
+
+	public static function setCacheStatements($cache) {
+		self::$cacheStatements=true==$cache;
+	}
+
+	public static function isCacheStatements() {
+		return self::$cacheStatements;
+	}
