afb3d67540918efd963396068ff66501b29ee32d
poison <rc.poison@gmail.com>
2009-07-21 20:42:07 +0200

    ADD: php function get get list of modified field ids

--- src/org/afraid/poison/db2php/generator/snippets/CodeGenerator.modificationTracking.php
+++ src/org/afraid/poison/db2php/generator/snippets/CodeGenerator.modificationTracking.php
@@ -37,6 +37,26 @@
 	}
 
 	/**
+	 * return array with the field ids of values which have been changed since the last notifyPristine call
+	 *
+	 * @return array
+	 */
+	public function getFieldsChanged() {
+		$changed=array();
+		if (!$this->isChanged()) {
+			return $changed;
+		}
+		$old=$this->getOldInstance()->toArray();
+		$new=$this->toArray();
+		foreach ($old as $fieldId=>$value) {
+			if ($new[$fieldId]!==$value) {
+				$changed[]=$fieldId;
+			}
+		}
+		return $changed;
+	}
+
+	/**
 	 * set this instance into pristine state
 	 */
 	public function notifyPristine() {
