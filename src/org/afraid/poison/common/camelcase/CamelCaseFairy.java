3fea16c7829193a791943f341a550a708f9a9216
poison <rc.poison@gmail.com>
2009-07-23 21:20:12 +0200

    ADD: first working version of fairy

--- src/org/afraid/poison/common/camelcase/CamelCaseFairy.java
+++ src/org/afraid/poison/common/camelcase/CamelCaseFairy.java
@@ -2,36 +2,47 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.afraid.poison.db2php.camelcase;
+package org.afraid.poison.common.camelcase;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.afraid.poison.common.FileUtil;
+import org.afraid.poison.common.IOUtil;
+import org.afraid.poison.common.StringUtil;
 
 /**
+ * The CamelCase fairy will sprinkle magic dust on strings.
+ * If you believe hard enough in it they will be correctly converted to CamelCase.
  *
  * @author poison
  */
 public class CamelCaseFairy {
 
-	
 	private Set<String> dictionary=null;
 
 	private Set<String> getDictionary() {
 		if (null==dictionary) {
-			BufferedReader in=null;
 			dictionary=new LinkedHashSet<String>();
+			InputStream in=null;
+			BufferedReader br=null;
 			try {
 				// aspell dump master english|grep -Pi '^[a-z]{2,}$'|tr [A-Z] [a-z]|sort|uniq|awk '{ print length(), $0 | "sort -rn" }'|awk '{ print $2}'
-				in=new BufferedReader(new FileReader("/home/andreas.schnaiter/files/www/wordlist.en"));
+				String path=new StringBuilder(FileUtil.getPackagePath(getClass())).append("/wordlist.en").toString();
+				//String path=new StringBuilder("/").append(getClass().getPackage().getName().replace(".", "/")).append("/wordlist.en").toString();
+				System.err.println(path);
+				in=getClass().getResourceAsStream(path);
+
+				br=new BufferedReader(new InputStreamReader(in));
 				String word;
-				while (null!=(word=in.readLine())) {
+				while (null!=(word=br.readLine())) {
 					dictionary.add(word);
 				}
 			} catch (FileNotFoundException ex) {
@@ -39,11 +50,8 @@ public class CamelCaseFairy {
 			} catch (IOException ex) {
 				Logger.getLogger(CamelCaseFairy.class.getName()).log(Level.SEVERE, null, ex);
 			} finally {
-				try {
-					in.close();
-				} catch (IOException ex) {
-					Logger.getLogger(CamelCaseFairy.class.getName()).log(Level.SEVERE, null, ex);
-				}
+				IOUtil.closeQuietly(br);
+				IOUtil.closeQuietly(in);
 			}
 		}
 		return dictionary;
@@ -51,18 +59,28 @@ public class CamelCaseFairy {
 
 	public String toCamelCase(String s) {
 		ArrayList<String> allContained=new ArrayList<String>();
+		StringBuilder sb=new StringBuilder(s.toLowerCase());
 		int pos;
-		StringBuilder sb=new StringBuilder(s);
 		for (String word : getDictionary()) {
 			pos=sb.indexOf(word);
 			if (-1!=pos) {
 				allContained.add(word);
-				sb.replace(pos, pos+word.length()-1, "");
+				sb.delete(pos, pos+word.length());
 				if (0==sb.length()) {
 					break;
 				}
 			}
 		}
+		for (String cw : allContained) {
+			/*
+			pos=s.indexOf(cw);
+			if (0!=pos) {
+			s=s.replace(cw, StringUtil.firstCharToUpperCase(cw));
+			}
+			 */
+			s=s.replace(cw, StringUtil.firstCharToUpperCase(cw));
+		}
+		System.err.println(allContained);
 
 		return s;
 	}
