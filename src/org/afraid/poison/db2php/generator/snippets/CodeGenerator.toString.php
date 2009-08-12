c1d18b581f47ec782e88ca850eb05105c3c643ad
poison <rc.poison@gmail.com>
2009-08-12 20:32:54 +0200

    ADD: toString

---
+++ src/org/afraid/poison/db2php/generator/snippets/CodeGenerator.toString.php
@@ -0,0 +1,14 @@
+
+
+	/**
+	 * get object as string
+	 *
+	 * @return string
+	 */
+	public function __toString() {
+		$s=null;
+		foreach ($this->toHash() as $fieldName=>$value) {
+			$s.=$fieldName . ": " . $value . "\n";
+		}
+		return $s;
+	}
