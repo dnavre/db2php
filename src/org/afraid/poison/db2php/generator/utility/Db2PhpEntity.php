b39094ff8615ab17eaf09584bf9a4ed96c72fc51
poison <rc.poison@gmail.com>
2009-08-13 19:19:55 +0200

    ADD: utility interfaces

---
+++ src/org/afraid/poison/db2php/generator/utility/Db2PhpEntity.php
@@ -0,0 +1,112 @@
+<?php
+/**
+* Copyright (c) 2009, Andreas Schnaiter
+*
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright notice,
+*    this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright notice,
+*    this list of conditions and the following disclaimer in the documentation
+*    and/or other materials provided with the distribution.
+* 3. Neither the name of  Andreas Schnaiter nor the names
+*    of its contributors may be used to endorse or promote products derived
+*    from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/**
+ *
+ * @author poison
+ */
+interface Db2PhpEntity {
+	
+	/**
+	 * Insert this instance into the database
+	 *
+	 * @param PDO $db
+	 * @return mixed
+	 */
+	public function insertIntoDatabase(PDO $db);
+
+	/**
+	 * Update this instance into the database
+	 *
+	 * @param PDO $db
+	 * @return mixed
+	 */
+	public function updateToDatabase(PDO $db);
+	
+	/**
+	 * Delete this instance from the database
+	 *
+	 * @param PDO $db
+	 * @return mixed
+	 */
+	public function deleteFromDatabase(PDO $db);
+
+	/**
+	 * Assign default values according to table
+	 *
+	 */
+    public function assignDefaultValues();
+
+	/**
+	 * Assign values from array with the field id as index and the value as value
+	 *
+	 * @param array $array
+	 */
+	public function assignByArray($array);
+	
+	/**
+	 *
+	 * Assign values from hash where the indexes match the tables field names
+	 *
+	 * @param array $result
+	 */
+	public function assignByHash($result);
+
+	/**
+	 * return array with the field id as index and the field value as value for the identifier fields.
+	 *
+	 * @return array
+	 */
+	public function getPrimaryKeyValues();
+
+	/**
+	 * return hash with the field name as index and the field value as value.
+	 *
+	 * @return array
+	 */
+	public function toHash();
+
+	/**
+	 * return array with the field id as index and the field value as value.
+	 *
+	 * @return array
+	 */
+	public function toArray();
+
+	/**
+	 * get element as DOM Document
+	 *
+	 * @return DOMDocument
+	 */
+	public function toDOM();
+
+}
+?>
