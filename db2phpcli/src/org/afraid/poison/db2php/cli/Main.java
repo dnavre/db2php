7ed1017ff1ea1f5b137ca2fec075d745e7aa57ef
poison <rc.poison@gmail.com>
2010-06-16 14:20:33 +0200

    CHG: move to dedicated dir

---
+++ db2phpcli/src/org/afraid/poison/db2php/cli/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.afraid.poison.db2php.cli;
+
+/**
+ *
+ * @author Andreas Schnaiter <as@euro-solutions.de>
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
